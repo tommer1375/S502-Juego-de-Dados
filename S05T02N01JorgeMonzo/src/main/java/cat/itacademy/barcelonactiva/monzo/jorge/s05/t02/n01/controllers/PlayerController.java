7e71fdc54dc2a8e991a0afe5a85ba54b3bb5743d
Jorge Monzó <151056618+tommer1375@users.noreply.github.com>
2024-04-28 19:21:10 +0200

    Add files via upload

---
+++ S05T02N01JorgeMonzo/src/main/java/cat/itacademy/barcelonactiva/monzo/jorge/s05/t02/n01/controllers/PlayerController.java
@@ -0,0 +1,36 @@
+package cat.itacademy.barcelonactiva.monzo.jorge.s05.t02.n01.controllers;
+
+
+import cat.itacademy.barcelonactiva.monzo.jorge.s05.t02.n01.model.dto.PlayerDTO;
+import cat.itacademy.barcelonactiva.monzo.jorge.s05.t02.n01.model.services.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/players")
+public class PlayerController {
+    @Autowired
+    private PlayerService playerService;
+
+    @PostMapping
+    public ResponseEntity<PlayerDTO> addPlayer(@RequestBody PlayerDTO playerDTO) {
+        PlayerDTO newPlayer = playerService.addPlayer(playerDTO);
+        return new ResponseEntity<>(newPlayer, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updatePlayer(@PathVariable("id") int id, @RequestBody PlayerDTO playerDTO) {
+        PlayerDTO playertoUpadate = playerService.updatePlayer(id, playerDTO);
+        return new ResponseEntity<>(playertoUpadate, HttpStatus.OK);
+    }
+    @GetMapping("/getAll")
+    public ResponseEntity<?> getAllPlayers() {
+        List<PlayerDTO> searchedPlayer = playerService.getAllPlayers();
+        return ResponseEntity.status(HttpStatus.FOUND).body(searchedPlayer);
+
+    }
+}
