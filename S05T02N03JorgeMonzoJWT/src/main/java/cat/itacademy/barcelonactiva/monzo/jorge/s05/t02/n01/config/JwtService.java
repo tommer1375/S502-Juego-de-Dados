04d3492f88112bb80d589dd163ce881073a9eda7
Jorge Monzó <151056618+tommer1375@users.noreply.github.com>
2024-04-29 18:15:24 +0200

    Update JwtService.java

--- S05T02N03JorgeMonzoJWT/src/main/java/cat/itacademy/barcelonactiva/monzo/jorge/s05/t02/n01/config/JwtService.java
+++ S05T02N03JorgeMonzoJWT/src/main/java/cat/itacademy/barcelonactiva/monzo/jorge/s05/t02/n01/config/JwtService.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Service;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "";
+    private static final String SECRET_KEY = "18C85BD7369C09B7F7F402EABC33B71BC0B938CAEE9E6E6C507290D06CF6DF7C";
 }
