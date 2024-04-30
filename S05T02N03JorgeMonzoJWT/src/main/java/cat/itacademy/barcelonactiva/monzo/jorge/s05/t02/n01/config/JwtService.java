a7ddd90bfce2a717f519d6255592147dc1420737
Jorge Monzó <151056618+tommer1375@users.noreply.github.com>
2024-04-30 13:39:01 +0200

    Delete S05T02N03JorgeMonzoJWT directory

--- S05T02N03JorgeMonzoJWT/src/main/java/cat/itacademy/barcelonactiva/monzo/jorge/s05/t02/n01/config/JwtService.java
+++
@@ -1,9 +0,0 @@
-package cat.itacademy.barcelonactiva.monzo.jorge.s05.t02.n01.config;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class JwtService {
-
-    private static final String SECRET_KEY = "18C85BD7369C09B7F7F402EABC33B71BC0B938CAEE9E6E6C507290D06CF6DF7C";
-}
--- S05T02N03JorgeMonzoJWT/src/main/java/cat/itacademy/barcelonactiva/monzo/jorge/s05/t02/n01/exceptions/PlayerAlreadyExists.java
+++
@@ -1,8 +0,0 @@
-package cat.itacademy.barcelonactiva.monzo.jorge.s05.t02.n01.exceptions;
-
-
-public class PlayerAlreadyExists extends RuntimeException{
-    public PlayerAlreadyExists(String message) {
-        super(message);
-    }
-}
