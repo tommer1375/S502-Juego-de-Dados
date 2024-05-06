ec2d0f7848389162b8fe77a0b17ba451507170d4
Jorge Monzó <151056618+tommer1375@users.noreply.github.com>
2024-05-06 13:18:57 +0200

    Delete S05T02N02MonzoJorge directory

--- S05T02N02MonzoJorge/src/main/java/cat/itacademy/barcelonactiva/monzo/jorge/s05/t02/n02/exceptions/PlayerAlreadyExists.java
+++
@@ -1,7 +0,0 @@
-package cat.itacademy.barcelonactiva.monzo.jorge.s05.t02.n02.exceptions;
-
-public class PlayerAlreadyExists extends RuntimeException{
-    public PlayerAlreadyExists(String message) {
-        super(message);
-    }
-}
--- S05T02N02MonzoJorge/src/main/java/cat/itacademy/barcelonactiva/monzo/jorge/s05/t02/n02/exceptions/PlayerNotFound.java
+++
@@ -1,7 +0,0 @@
-package cat.itacademy.barcelonactiva.monzo.jorge.s05.t02.n02.exceptions;
-
-public class PlayerNotFound extends RuntimeException{
-    public PlayerNotFound(String message) {
-        super(message);
-    }
-}
