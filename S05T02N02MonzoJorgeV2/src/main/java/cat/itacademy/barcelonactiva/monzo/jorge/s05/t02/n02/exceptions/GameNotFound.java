abe54f8eb09b396e28d6b34557fbbc0e390aea8f
Jorge Monzó <151056618+tommer1375@users.noreply.github.com>
2024-05-06 13:25:31 +0200

    Delete S05T02N02MonzoJorgeV2 directory

--- S05T02N02MonzoJorgeV2/src/main/java/cat/itacademy/barcelonactiva/monzo/jorge/s05/t02/n02/exceptions/GameNotFound.java
+++
@@ -1,9 +0,0 @@
-package cat.itacademy.barcelonactiva.monzo.jorge.s05.t02.n02.exceptions;
-
-
-public class GameNotFound extends RuntimeException{
-
-    public GameNotFound(String message) {
-        super(message);
-    }
-}
