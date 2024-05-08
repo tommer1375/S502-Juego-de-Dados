6ede9fd672542a93c01dbe35e1deafbd66e66d75
Jorge Monzó <151056618+tommer1375@users.noreply.github.com>
2024-05-08 11:21:46 +0200

    Delete S05T02N03JorgeMonzoJWTV2 directory

--- S05T02N03JorgeMonzoJWTV2/src/main/java/cat/itacademy/barcelonactiva/monzo/jorge/s05/t02/n01/exceptions/UserAlreadyExists.java
+++
@@ -1,9 +0,0 @@
-package cat.itacademy.barcelonactiva.monzo.jorge.s05.t02.n01.exceptions;
-
-public class UserAlreadyExists extends RuntimeException{
-
-    public UserAlreadyExists(String message) {
-        super(message);
-    }
-}
-
--- S05T02N03JorgeMonzoJWTV2/src/main/java/cat/itacademy/barcelonactiva/monzo/jorge/s05/t02/n01/exceptions/UserNotFound.java
+++
@@ -1,9 +0,0 @@
-package cat.itacademy.barcelonactiva.monzo.jorge.s05.t02.n01.exceptions;
-
-public class UserNotFound extends RuntimeException{
-
-    public UserNotFound(String message) {
-        super(message);
-    }
-}
-
