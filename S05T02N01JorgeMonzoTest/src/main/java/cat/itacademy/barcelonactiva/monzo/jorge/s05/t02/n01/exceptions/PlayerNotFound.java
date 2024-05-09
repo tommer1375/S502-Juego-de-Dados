4ed0041545859d82daff2f796a70c22cb1900463
Jorge Monzó <151056618+tommer1375@users.noreply.github.com>
2024-05-09 10:41:40 +0200

    Delete S05T02N01JorgeMonzoTest directory

--- S05T02N01JorgeMonzoTest/src/main/java/cat/itacademy/barcelonactiva/monzo/jorge/s05/t02/n01/exceptions/PlayerNotFound.java
+++
@@ -1,8 +0,0 @@
-package cat.itacademy.barcelonactiva.monzo.jorge.s05.t02.n01.exceptions;
-
-
-public class PlayerNotFound extends RuntimeException{
-    public PlayerNotFound(String message) {
-        super(message);
-    }
-}
