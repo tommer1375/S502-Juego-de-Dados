25df7d3c7ad5a1fa7e9306267e76e79cf31714d4
Jorge Monzó <151056618+tommer1375@users.noreply.github.com>
2024-05-13 10:53:31 +0200

    Delete S05T02N03JorgeMonzoJWT directory

--- S05T02N03JorgeMonzoJWT/src/main/java/cat/itacademy/barcelonactiva/monzo/jorge/s05/t02/n01/exceptions/GameNotFound.java
+++
@@ -1,8 +0,0 @@
-package cat.itacademy.barcelonactiva.monzo.jorge.s05.t02.n01.exceptions;
-
-public class GameNotFound extends RuntimeException{
-
-    public GameNotFound(String message) {
-        super(message);
-    }
-}
--- S05T02N03JorgeMonzoJWT/src/main/java/cat/itacademy/barcelonactiva/monzo/jorge/s05/t02/n01/model/domain/Role.java
+++
@@ -1,6 +0,0 @@
-package cat.itacademy.barcelonactiva.monzo.jorge.s05.t02.n01.model.domain;
-
-public enum Role {
-    USER,
-    ADMIN;
-}
